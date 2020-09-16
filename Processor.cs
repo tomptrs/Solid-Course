569c3dc7ec3fea44eeacba7bedea86c8efff67ae
Tom Peeters <tom_ptrs@yahoo.com>
2020-09-16 09:16:23 +0200

    json reader

---
+++ Processor.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace SOLID_Start
+{
+    class Processor
+    {
+
+        public void Process()
+        {
+            List<Klant> klanten = new List<Klant>();
+
+            string json = File.ReadAllText("./peeters.json"); 
+            var peeters = JsonConvert.DeserializeObject<Klant>(json, new StringEnumConverter());
+            if(String.IsNullOrEmpty(peeters.Naam))
+            {
+                Console.WriteLine("Klant moet een naam hebben");
+            }           
+            peeters.AddMovie(new Huur(new Movie("Godfather", 1), 3));
+            klanten.Add(peeters);
+
+            json = File.ReadAllText("./vandeperre.json");
+            var vandeperre = JsonConvert.DeserializeObject<Klant>(json, new StringEnumConverter());
+            if (String.IsNullOrEmpty(vandeperre.Naam))
+            {
+                Console.WriteLine("Klant moet een naam hebben");
+            }
+            vandeperre.AddMovie(new Huur(new Movie("Lion King", 2), 2));
+            klanten.Add(vandeperre);
+
+
+            json = File.ReadAllText("./verlinden.json");
+            var verlinden = JsonConvert.DeserializeObject<Klant>(json, new StringEnumConverter());
+            if (String.IsNullOrEmpty(verlinden.Naam))
+            {
+                Console.WriteLine("Klant moet een naam hebben");
+            }
+            verlinden.AddMovie(new Huur(new Movie("Rundskop", 1), 4));
+            klanten.Add(verlinden);
+
+
+            json = File.ReadAllText("./dams.json");
+            var dams = JsonConvert.DeserializeObject<Klant>(json, new StringEnumConverter());
+            if (String.IsNullOrEmpty(dams.Naam))
+            {
+                Console.WriteLine("Klant moet een naam hebben");
+            }
+            dams.AddMovie(new Huur(new Movie("Top Gun", 3), 1));
+            klanten.Add(dams);
+
+            Console.WriteLine("start berekenen prijs");
+            foreach (Klant klant in klanten)
+            {
+                Console.WriteLine(klant.GetRekening());
+                SendComfirmationMessage(klant);
+            }
+            Console.WriteLine("einde berekening...");
+
+            Console.ReadLine();
+        }
+
+        private void SendComfirmationMessage(Klant klant)
+        {
+            Console.WriteLine("stuur een mail naar de klant");
+            Console.WriteLine("SENDING MAIL...");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,32 +9,8 @@ namespace SOLID_Start
         {
             //Demo-Application
 
-            List<Klant> klanten = new List<Klant>();
-            Klant k = new Klant("Peeters");
-
-            k.AddMovie(new Huur(new Movie("Godfather", 1), 3));
-            klanten.Add(k);
-
-            Klant k2 = new Klant("Vandeperre");
-            k2.AddMovie(new Huur(new Movie("Lion King", 2), 2));
-            klanten.Add(k2);
-
-
-            Klant c3 = new Klant("Verlinden");
-            c3.AddMovie(new Huur(new Movie("Rundskop", 1), 4));
-            klanten.Add(c3);
-
-
-            Klant c4 = new Klant("Dams");
-            c4.AddMovie(new Huur(new Movie("Top Gun", 3), 1));
-            klanten.Add(c4);
-
-            foreach (Klant klant in klanten)
-            {
-                Console.WriteLine(klant.GetRekening());
-            }
-
-            Console.ReadLine();
+            Processor processor = new Processor();
+            processor.Process();
         }
     }
 }
